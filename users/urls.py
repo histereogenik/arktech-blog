c214aeee101b88d79f110721cc8f61ccc68e95a0
Humberto Nacif <gigantedeluz@gmail.com>
2025-05-08 16:13:45 -0300

    feat: adding basename to posts users urls

--- users/urls.py
+++ users/urls.py
@@ -3,7 +3,7 @@ from rest_framework.routers import DefaultRouter
 from users.views import UserViewSet
 
 router = DefaultRouter()
-router.register(r'users', UserViewSet)
+router.register(r'users', UserViewSet, basename='user')
 
 urlpatterns = [
     path('', include(router.urls)),
